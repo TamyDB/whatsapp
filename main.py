755e563338a05e902ba541cdaab11fcadd4954a6
Rafael Tamy <rafaeltamydb@gmail.com>
2025-06-01 11:45:47 -0300

    minor fixes

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ with open('./contact_list.csv', newline='') as csvfile:
     next(contacts)
     for contact in contacts:
         number = ''.join(contact)
-        pywhatkit.sendwhatmsg_instantly(number, 'Hello World', 15, True, 4)
-        time.sleep(15)
+        pywhatkit.sendwhatmsg_instantly(number, 'Bom dia!! Já ouviu falar da Coffe Day? ' \
+        'Aqui temos café de qualidade e lanches para alegrar sua tarde.', 15, True, 4)
+        time.sleep(10)
+    
         
         
 
